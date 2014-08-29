e8aec654b075636d74020a9ca443a203425c5c3a
djc218 <djc218@lehigh.edu>
2014-08-29 13:14:54 +0000

    HelloWorld updated

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+//Daniel Colville
+//defines class for Hello World Program
+public class HelloWorld{
+    //main method 
+    public static void main(String[] args) {
+    //print out Hello World with a box around it then test below that
+        System.out.println("#######################") ;
+        System.out.println("#######################") ;
+        System.out.println("#                     #") ; 
+        System.out.println("#     Hello World!!   #") ;
+        System.out.println("#                     #") ;
+        System.out.println("#######################") ;
+        System.out.println("#######################") ;
+        System.out.println("test");
+    }
+}

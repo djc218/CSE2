c5048011e59a9e2bdbe42e84a85c7c87f239e953
djc218 <djc218@lehigh.edu>
2014-09-01 15:23:25 +0000

    Welcome Program Submission

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+/*
+Daniel Colville
+Create a program that prints a greeting including Lehigh ID and 
+a personal fact 
+*/
+public class WelcomeClass{
+    //main method
+    public static void main(String[] args) {
+    System.out.println("    -----------");
+    System.out.println("    | WELCOME |");
+    System.out.println("    -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-D--J--C--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("This summer I visited the National Baseball Hall of");
+    System.out.println("Fame with my family.");
+    }
+}    
